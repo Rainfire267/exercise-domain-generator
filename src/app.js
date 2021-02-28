89e54376e72cdd50f5a65c2636d4d3889056c7e3
Rainfire267 <ejimenez267@gmail.com>
2021-02-28 23:49:16 +0000

    primer intento

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dotExt = [".com", ".net", ".us", ".io"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let cuatroRandom = [];
+  let posicionesElegibles = [];
+  let i, r;
+  for (i = 0; i < pronoun.length; i++) posicionesElegibles[i] = i;
+  for (i = 0; i < 4; i++) {
+    r = Math.floor(Math.random() * posicionesElegibles.length);
+    cuatroRandom.push(pronoun[posicionesElegibles[r]]);
+    posicionesElegibles.splice(r, 1);
+  }
+  console.log(cuatroRandom.toString());
 };
+/*let pronRandom = Math.floor(Math.random() * pronoun.length);
+  let adjRandom = Math.floor(Math.random() * adj.length);
+  let nounRandom = Math.floor(Math.random() * noun.length);
+  let dotExtRandom = Math.floor(this.Math.random() * dotExt.length);
+
+  let ranDomain = [pronRandom + adjRandom + nounRandom + dotExtRandom];
+
+  ranDomain = document.getElementById("ranDomainGen");
+  ranDomain.innerHTML = pronRandom + adjRandom + nounRandom + dotExtRandom;
+  */
